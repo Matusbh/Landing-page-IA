f8d582c6c6122da74457cf9b90aa231a513f40a4
Matus b <matusbehun03@gmail.com>
2025-11-15 16:54:33 +0000

    En español he cambiado lo snombres de los deportes que se pueden hacer c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+      <link rel="icon" type="image/svg+xml" href="src/app/home.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
