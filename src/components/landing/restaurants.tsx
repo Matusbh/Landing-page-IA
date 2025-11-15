cf099d8c8c913f3e710cbcbcc358f3ace7ce468c
Matus b <matusbehun03@gmail.com>
2025-11-15 08:26:16 +0000

    Como  ves he hecho cambios en es.json aplica los cambios que hice yo al

--- src/components/landing/restaurants.tsx
+++ src/components/landing/restaurants.tsx
@@ -6,7 +6,7 @@ import { UtensilsCrossed } from 'lucide-react'
 
 const restaurantLinks = [
   "https://www.google.com/maps/search/?api=1&query=Restaurante+Saúco+Alcalá",
-  "https://www.google.com/maps/search/?api=1&query=Restaurante+El+Pescador+Alcalá",
+  "https://www.google.com/maps/place/Cafeter%C3%ADa+Alcal%C3%A1+Center/@28.2015542,-16.8300292,486m/data=!3m1!1e3!4m10!1m2!2m1!1scafeter%C3%ADa+cerca+de+Centeno,+Alcal%C3%A1!3m6!1s0xc6a8dc048a67cd3:0xa5bbae4a39590125!8m2!3d28.2010801!4d-16.8262301!15sCiRjYWZldGVyw61hIGNlcmNhIGRlIENlbnRlbm8sIEFsY2Fsw6FaJSIjY2FmZXRlcsOtYSBjZXJjYSBkZSBjZW50ZW5vIGFsY2Fsw6GSAQRjYWZlmgEjQ2haRFNVaE5NRzluUzBWSlEwRm5TVVJ3YTFCNVIwMUJFQUXgAQD6AQQIABA9!16s%2Fg%2F11t426tk53?entry=ttu&g_ep=EgoyMDI1MTExMi4wIKXMDSoASAFQAw%3D%3D",
   "https://www.google.com/maps/place/Restaurante+Muelle+Viejo/@28.2014956,-16.8347091,973m/data=!3m2!1e3!4b1!4m6!3m5!1s0xc6a8de65d576da5:0xd307221072a7140d!8m2!3d28.201491!4d-16.8298382!16s%2Fg%2F11g2_z64j6?entry=ttu&g_ep=EgoyMDI1MTExMS4wIKXMDSoASAFQAw%3D%3D"
 ]
 
