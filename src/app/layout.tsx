21ad02a8cfea0d2497a4d9018bdad5c8759173ce
Matus b <matusbehun03@gmail.com>
2025-12-09 19:22:17 +0000

    Si se ve muy pequeño el favicon como lo hago un poco mas grande

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
   title: 'DioVista Apartments',
   description: 'Tu Escapada Soñada en Alcalá',
   icons: {
-    icon: '/images/Diovista-svg.svg',
+    icon: '/images/favicon.ico',
     shortcut: '/images/Diovista-svg.svg',
-    apple: '/images/Diovista-svg.svg',
+    apple: '/images/apple-icon.png',
   },
 };
 
